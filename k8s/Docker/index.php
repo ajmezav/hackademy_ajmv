6d14bebb471e3e01812dad79c1f94abc6e358b5c
ajmeza <alvaro.mesa@mercadolibre.com.co>
2022-10-12 19:46:04 -0500

    se actualiza repo

--- k8s/Docker/index.php
+++ k8s/Docker/index.php
@@ -1,10 +1,10 @@
 <html>
  <head>
   <title>
-Hola Hackademy Class  </title>
+Hola Hackademy Class - 12 Oct 20:42 </title>
  </head>
  <body>
  <?php echo '<p>
-Hola Hackademy Class </p>'; ?>
+Hola Hackademy Class - 12 Oct 20:42</p>'; ?>
  </body>
 </html>
